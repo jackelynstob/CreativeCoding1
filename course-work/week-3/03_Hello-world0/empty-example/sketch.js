aa0e09c6dd1ebf062edba316b494f23b29a7ec16
Jack Stob <jackelynstob23@gmail.com>
2017-09-26 16:39:39 -0600

    Added p5 examples in the coursework for HW-3

---
+++ course-work/week-3/03_Hello-world0/empty-example/sketch.js
@@ -0,0 +1,31 @@
+/*
+name: sketch.js
+last modified: September 26
+
+author: Jack Stob
+this is the sketch.js file for the "Hello World" project
+ */
+
+// This is the setup() function definition
+//This is required by p5 for setup
+function setup(){
+}
+
+//This is the draw() function definition
+//This gets called after setup() finishes executing
+function draw(){
+
+  //This will draw a circle
+  //centered at point (20,20)
+  //This circle will have a diameter of 30 px
+    ellipse( 20, 20, 30, 30 ); // everything after these slashes is ignored
+
+    ellipse (20, 20, 30, 30);
+    // ellipse (200, 20, 30, 30);
+    // ellipse (20, 200, 30, 30);
+    // ellipse (20, 20, 300, 30);
+    // ellipse (20, 20, 30, 300);
+
+}
+
+// TODO: Remember to push this all to your GitHub AFTER recording the videos
---
+++ hw-3/empty-example/sketch.js
@@ -0,0 +1,24 @@
+// start with the setup() function
+function setup() {
+
+      // canvas should be created at the beginning
+      // make a canvas 800 in width and 450 in height
+      createCanvas( 800, 450 );
+
+      // create a gold background
+
+      background('gold');
+
+}
+
+
+// Declare draw() function IMPORTANT
+
+function draw() {
+
+  // use the text function, write "Hello World" and put in the top middle of screen
+
+  text("Hello World!", 375, 100);
+
+
+}
